49aa3f6006474e0d7f247c509ffd440911a0ba46
Ao Wang <aw3338@drexel.edu>
2019-07-19 23:46:46 -0400

    Less awkward algorithm
    Does not require the lowercase alphabet string

--- Java-Programming-Arrays-Lists-and-Structured-Data-master/Week1/CaesarCipher.java
+++ Java-Programming-Arrays-Lists-and-Structured-Data-master/Week1/CaesarCipher.java
@@ -5,29 +5,29 @@ public class CaesarCipher {
      public String encrypt(String input, int key){
         // creates stringbuilder object to hold input
         StringBuilder encrypted = new StringBuilder(input);
-        String alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+        String alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
         // shifting the alphabet by key amount
-        String shiftedAlphabetUpper = alphabetUpper.substring(key) + alphabetUpper.substring(0, key);
-        String shiftedAlphabetLower = alphabetLower.substring(key) + alphabetLower.substring(0, key);
+        String shiftedAlphabet= alphabet.substring(key) + alphabet.substring(0, key);
+        
         // goes through each letter in encrypted
         for(int i = 0; i < encrypted.length(); i++){
             char currChar = encrypted.charAt(i);  
             int idx;
             // find index of the current letter in the original alphabet
             if(Character.isUpperCase(currChar)){
-                idx = alphabetUpper.indexOf(currChar); 
+                idx = alphabet.indexOf(currChar); 
             }else{
-                idx = alphabetLower.indexOf(currChar); 
+                idx = alphabet.indexOf(Character.toUpperCase(currChar)); 
             }
             // if index is found, use the index from the original alphabet
             // to get the letter from the encrypted alphabet
             if(idx != -1){
                 char newChar;
                 if(Character.isUpperCase(currChar)){
-                    newChar = shiftedAlphabetUpper.charAt(idx);     
+                    newChar = shiftedAlphabet.charAt(idx);     
                 }else{
-                    newChar = shiftedAlphabetLower.charAt(idx); 
+                    newChar = Character.toLowerCase(shiftedAlphabet.charAt(idx)); 
                 }
                 // set the letters in the stringbuilder object with the
                 // letters in the encrypted alphabet
