a918fcec6d41f6368d8285e5fa3ea93d0e238278
Ao Wang <aw3338@drexel.edu>
2019-07-28 00:11:02 -0400

    Update VerbsAndFruits.java

--- Java-Programming-Arrays-Lists-and-Structured-Data-master/Week2/Programming Exercise: Using GladLibs/VerbsAndFruits.java
+++ Java-Programming-Arrays-Lists-and-Structured-Data-master/Week2/Programming Exercise: Using GladLibs/VerbsAndFruits.java
@@ -2,6 +2,7 @@ import edu.duke.*;
 import java.util.*;
 
 public class GladLib {
+    // declaring ArrayLists for each of the brackets in the gladlib txt files
     private ArrayList<String> adjectiveList;
     private ArrayList<String> nounList;
     private ArrayList<String> colorList;
@@ -11,29 +12,28 @@ public class GladLib {
     private ArrayList<String> timeList;
     private ArrayList<String> verbList;
     private ArrayList<String> fruitList;
+    
     private ArrayList<String> usedWords;
-    private int wordAlreadyUsed;
-
+    private int repeatedWords;
     private Random myRandom;
 
     private static String dataSourceURL = "http://dukelearntoprogram.com/course3/data";
     private static String dataSourceDirectory = "data";
-
-    // emptry constructor defaults to the duke URL for data
+    
     public GladLib(){
         initializeFromSource(dataSourceDirectory);
         myRandom = new Random();
+        usedWords = new ArrayList<>();
+        repeatedWords = 0;
     }
 
     public GladLib(String source){
         initializeFromSource(source);
-        usedWords = new ArrayList<>();
-        wordAlreadyUsed = 0;
         myRandom = new Random();
+        usedWords = new ArrayList<>();
+        repeatedWords = 0;
     }
-
-    // from the method readIt, all the arraylists are stored with 
-    // arraylists stored with each line of specified files
+    
     private void initializeFromSource(String source) {
         adjectiveList= readIt(source+"/adjective.txt"); 
         nounList = readIt(source+"/noun.txt");
@@ -45,14 +45,13 @@ public class GladLib {
         verbList = readIt(source+ "/verb.txt");
         fruitList = readIt(source+ "/fruit.txt");
     }
-    // chooses a random number from the size of the arraylist and returns it
+    
+    // returns a random string from the individual txt files 
     private String randomFrom(ArrayList<String> source){
         int index = myRandom.nextInt(source.size());
         return source.get(index);
     }
-
-    // if label equals one of the titles of the file, get a random
-    // word from it
+    
     private String getSubstitute(String label) {
         if (label.equals("country")) {
             return randomFrom(countryList);
@@ -75,21 +74,20 @@ public class GladLib {
         if (label.equals("timeframe")){
             return randomFrom(timeList);
         }
-        if (label.equals("number")){
-            return ""+myRandom.nextInt(50)+5;
-        }
         if (label.equals("verb")){
             return randomFrom(verbList);
         }
         if (label.equals("fruit")){
             return randomFrom(fruitList);
         }
+        if (label.equals("number")){
+            return ""+myRandom.nextInt(50)+5;
+        }
         return "**UNKNOWN**";
     }
 
-    // checks each word line by line in the arraylists if surrounded by
-    // <>, places it with a random word in the file from the method, getSubstitute
-    // if word is picked more than once, word is chosen again till a different one
+    // returns the replaced word from the gladlib file from the
+    // specific lists
     private String processWord(String w){
         int first = w.indexOf("<");
         int last = w.indexOf(">",first);
@@ -101,11 +99,12 @@ public class GladLib {
         String sub = getSubstitute(w.substring(first+1,last));
         while(usedWords.contains(sub)){
             sub = getSubstitute(w.substring(first+1,last));
-            wordAlreadyUsed++;
+            repeatedWords++;
         }
-        usedWords.add(sub);
+        if(!usedWords.contains(sub)){
+            usedWords.add(sub);
+        }
         return prefix+sub+suffix;
-
     }
 
     private void printOut(String s, int lineWidth){
@@ -136,8 +135,7 @@ public class GladLib {
         }
         return story;
     }
-    // loops through the website's data or file data line by line
-    // stores each line into a string ArrayList
+
     private ArrayList<String> readIt(String source){
         ArrayList<String> list = new ArrayList<String>();
         if (source.startsWith("http")) {
@@ -159,7 +157,9 @@ public class GladLib {
         System.out.println("\n");
         String story = fromTemplate("data/madtemplate2.txt");
         printOut(story, 60);
-        System.out.println();
-        System.out.println("Words replaced: " + wordAlreadyUsed);
+        System.out.println("\nTotal number of words replaced: " + repeatedWords);
+        usedWords.clear();
+        repeatedWords = 0;
     }
+
 }
